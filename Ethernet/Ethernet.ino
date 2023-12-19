4d47bb926f70d9cb0992b09901a9a0301c83c26e
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2023-12-19 20:32:37 -0300

    v3

---
+++ Ethernet/Ethernet.ino
@@ -0,0 +1,15 @@
+#include <EEPROM.h>
+#include <SoftwareSerial.h>
+#include <Ethernet.h>
+
+SoftwareSerial receptor(0, 1);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
